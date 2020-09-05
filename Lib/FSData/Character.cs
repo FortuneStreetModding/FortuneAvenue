19d12b7049bab92199a5121d6464dc9cfaf176cf
Deflaktor <deflaktor@gmail.com>
2020-09-05 03:40:00 +0200

    Add Character Enum

---
+++ Lib/FSData/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSEditor.FSData
+{
+	public enum Character : UInt32
+	{
+		Mario = 0,
+		Luigi = 1,
+		Peach = 2,
+		Yoshi = 3,
+		Bowser = 4,
+		Toad = 5,
+		DonkeyKong = 6,
+		Wario = 7,
+		Waluigi = 8,
+		Daisy = 9,
+		Birdo = 10,
+		DiddyKong = 11,
+		BowserJr = 12,
+		Slime = 13,
+		Princessa = 14,
+		Kiryl = 15,
+		Yangus = 16,
+		Angelo = 17,
+		Platypunk = 18,
+		Bianca = 19,
+		Alena = 20,
+		Carver = 21,
+		Jessica = 22,
+		Dragonlord = 23,
+		Stella = 24,
+		Patty = 25
+	}
+}
