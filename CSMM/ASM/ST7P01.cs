7106d58bf49faf6d887ef229bbff09fa929db37d
Unknown <deflaktor@gmail.com>
2020-10-19 00:56:14 +0200

    Add WIP custom map defaults table hack

--- CSMM/ASM/ST7P01.cs
+++ CSMM/ASM/ST7P01.cs
@@ -1,4 +1,4 @@
-using MiscUtil.IO;
+﻿using MiscUtil.IO;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -31,12 +31,6 @@ namespace CustomStreetManager
             // r4 <- 0x80436bc0                                    -> r4 <- mapDescriptionTableAddr
             PowerPcAsm.Pair16Bit v = PowerPcAsm.make16bitValuePair(mapDescriptionTableAddr);
             stream.Seek(toFileAddress(0x80212164), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Seek(4, SeekOrigin.Current); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
-
-            // -- instead of having a map lock info text per map, make it generic -- 
-            // lwzx r3=>DWORD_80436c08,r3,r0                        ->   li r3,0x1153
-            // we relocate the map description table somewhere else, as such this hack is not needed anymore
-            // that is the reason why it is commented out here
-            // stream.Seek(toFileAddress(0x80212238), SeekOrigin.Begin); stream.Write(0x38601153);
         }
         bool ST7_Interface.isHackExtendedMapDescriptions(EndianBinaryReader stream, Func<uint, int> toFileAddress)
         {
@@ -169,14 +163,91 @@ namespace CustomStreetManager
             return (Int16)(PowerPcAsm.getOpcodeParameter(opcode) + 1);
         }
 
-        void ST7_Interface.writeHackExtendedMapDefaultsTable(EndianBinaryWriter stream, Func<uint, int> toFileAddress)
+        void ST7_Interface.writeHackMapDefaultGoalMoneyTable(EndianBinaryWriter stream, Func<uint, int> toFileAddress, Int16 tableSize, UInt32 tableAddr)
         {
-            throw new NotImplementedException();
+            UInt32 initMapGoalMoneyTourMode = 0x8020d160;
+            UInt32 getMapDefaultGoalMode = 0x80211c88;
+            PowerPcAsm.Pair16Bit v = PowerPcAsm.make16bitValuePair(tableAddr);
+
+            // subi r30,r30,0x15                                    -> nop
+            stream.Seek(toFileAddress(0x8020d0dc), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            stream.Seek(toFileAddress(initMapGoalMoneyTourMode), SeekOrigin.Begin);
+            // li r0,0x15       ->  nop
+            stream.Write(PowerPcAsm.nop());
+            stream.Seek(8, SeekOrigin.Current);
+            // mulli r0,r0,0x24 ->  mulli r0,r0,0x04
+            stream.Write(PowerPcAsm.mulli(0,0,4));
+            // r4 <- 804363c8   ->  r4 <- tableAddr
+            stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
+            // mulli r3,r30,0x24 ->  mulli r3,r30,0x04
+            stream.Write(PowerPcAsm.mulli(3, 30, 4));
+
+            // subi r31,r3,0x15                                    -> nop
+            stream.Seek(toFileAddress(0x80211c04), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            stream.Seek(toFileAddress(getMapDefaultGoalMode), SeekOrigin.Begin);
+            // li r3,0x15       ->  nop
+            stream.Write(PowerPcAsm.nop());
+            stream.Seek(8, SeekOrigin.Current);
+            // mulli r0,r3,0x24 ->  mulli r0,r3,0x04
+            stream.Write(PowerPcAsm.mulli(0, 3, 4));
+            // r4 <- 804363c8   ->  r4 <- tableAddr
+            stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
+            // mulli r3,r31,0x24 ->  mulli r3,r31,0x04
+            stream.Write(PowerPcAsm.mulli(3, 31, 4));
         }
 
-        bool ST7_Interface.isHackExtendedMapDefaultsTable(EndianBinaryReader stream, Func<uint, int> toFileAddress)
+        void ST7_Interface.writeHackExtendedMapDefaultsTable(EndianBinaryWriter stream, Func<uint, int> toFileAddress, Int16 mapDefaultsTableSize, UInt32 mapDefaultsTableAddr)
         {
-            throw new NotImplementedException();
+            // -- SetupWorldMap() --
+            // subi r3,r3,0x15                                     -> nop
+            stream.Seek(toFileAddress(0x8020cec8), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // cmpwi r3,0x12                                       -> cmpwi r3,mapDefaultsTableSize
+            stream.Seek(toFileAddress(0x8020ced4), SeekOrigin.Begin); stream.Write(PowerPcAsm.cmpwi(3, mapDefaultsTableSize));
+            // r3 <- 0x804363c8                                    -> r3 <- mapDefaultsTableAddr
+            PowerPcAsm.Pair16Bit v = PowerPcAsm.make16bitValuePair(mapDefaultsTableAddr);
+            stream.Seek(toFileAddress(0x8020cee4), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(3, v.upper16Bit)); stream.Seek(4, SeekOrigin.Current); stream.Write(PowerPcAsm.addi(3, 3, v.lower16Bit));
+            // li r0,0x15                                          -> nop
+            stream.Seek(toFileAddress(0x8020cf68), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+
+            // r4 <- 0x804363c8                                    -> r4 <- mapDefaultsTableAddr
+            stream.Seek(toFileAddress(0x8020d170), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
+            // li r0,0x15                                          -> nop
+            stream.Seek(toFileAddress(0x8020d248), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // r3 <- 0x804363c8                                    -> r3 <- mapDefaultsTableAddr
+            stream.Seek(toFileAddress(0x8020d258), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(3, v.upper16Bit)); stream.Write(PowerPcAsm.addi(3, 3, v.lower16Bit));
+            // r4 <- 0x804363c8                                    -> r4 <- mapDefaultsTableAddr
+            stream.Seek(toFileAddress(0x8020d344), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Seek(4, SeekOrigin.Current); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
+
+            // cmpwi r30,0x12                                      -> cmpwi r30,mapDefaultsTableSize
+            stream.Seek(toFileAddress(0x8020d0e8), SeekOrigin.Begin); stream.Write(PowerPcAsm.cmpwi(30, mapDefaultsTableSize));
+            // subi r30,r30,0x15                                   -> nop
+            stream.Seek(toFileAddress(0x8020d1c4), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // cmpwi r30,0x12                                      -> cmpwi r30,mapDefaultsTableSize
+            stream.Seek(toFileAddress(0x8020d1d0), SeekOrigin.Begin); stream.Write(PowerPcAsm.cmpwi(30, mapDefaultsTableSize));
+            // subi r30,r30,0x15                                   -> nop
+            stream.Seek(toFileAddress(0x8020d2b0), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // cmpwi r30,0x12                                      -> cmpwi r30,mapDefaultsTableSize
+            stream.Seek(toFileAddress(0x8020d2bc), SeekOrigin.Begin); stream.Write(PowerPcAsm.cmpwi(30, mapDefaultsTableSize));
+            // li r0,0x15                                          -> nop
+            stream.Seek(toFileAddress(0x8020d334), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // -- GetMapOpponents() --
+            // subi r4,r4,0x15                                     -> nop
+            stream.Seek(toFileAddress(0x80211af4), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+            // cmpwi r4,0x12                                       -> cmpwi r4,mapDefaultsTableSize
+            stream.Seek(toFileAddress(0x80211b00), SeekOrigin.Begin); stream.Write(PowerPcAsm.cmpwi(4, mapDefaultsTableSize));
+            // r4 <- 0x804363c8                                    -> r4 <- mapDefaultsTableAddr
+            stream.Seek(toFileAddress(0x80211b10), SeekOrigin.Begin); stream.Write(PowerPcAsm.lis(4, v.upper16Bit)); stream.Seek(4, SeekOrigin.Current); stream.Write(PowerPcAsm.addi(4, 4, v.lower16Bit));
+            // li r3,0x15                                          -> nop
+            stream.Seek(toFileAddress(0x80211b94), SeekOrigin.Begin); stream.Write(PowerPcAsm.nop());
+        }
+
+        UInt32 ST7_Interface.readMapDefaultsTableAddr(EndianBinaryReader stream, Func<uint, int> toFileAddress)
+        {
+            stream.Seek(toFileAddress(0x8020cee4), SeekOrigin.Begin);
+            var lis_opcode = stream.ReadUInt32();
+            stream.Seek(4, SeekOrigin.Current);
+            var addi_opcode = stream.ReadUInt32();
+            return PowerPcAsm.make32bitValueFromPair(lis_opcode, addi_opcode);
         }
 
         void ST7_Interface.writeHackExtendedMapSettingsTable(EndianBinaryWriter stream, Func<uint, int> toFileAddress)
