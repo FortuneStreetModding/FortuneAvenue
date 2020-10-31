f19f7bb88255ab286dd39c7422c0b20c55d3318f
Deflaktor <deflaktor@gmail.com>
2020-10-31 03:17:46 +0100

    Bump version number

--- Editor/AboutBox.cs
+++ Editor/AboutBox.cs
@@ -14,7 +14,7 @@ namespace Editor
             InitializeComponent();
             this.Text = String.Format("About {0}", "Fortune Avenue");
             this.labelProductName.Text = "Fortune Avenue";
-            this.labelVersion.Text = "Version 9 Beta 3";
+            this.labelVersion.Text = "Version 9 Beta 4";
             this.labelCopyright.Text = "© 2012, 2013, 2019, 2020";
             this.labelCompanyName.Text = "";
             this.textBoxDescription.Text = "Based on the Fortune Street Wii Board Editor begun by Xanares, Amazing Ampharos, and zerozerozerozer.\r\nFurther work done by STJrInuyasha, Raspberryfloof, Deflaktor, Nikkums.\r\n\r\nFortune Street is ™ and © Nintendo, ARMOR PROJECT, and SQUARE ENIX.";
