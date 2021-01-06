81dca9be59fbc14e5adc9e3263290b0b709758e2
Deflaktor <deflaktor@gmail.com>
2021-01-06 04:55:38 +0100

    Small progress on CLI

--- CSMM/CSMM.cs
+++ CSMM/CSMM.cs
@@ -1,12 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CustomStreetMapManager
 {
     class CSMM
     {
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                var ct = cancellationTokenSource.Token;
+                Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+                {
+                    // prevent terminating the application instantly if user requests it -> only once user requests twice
+                    if (!cancellationTokenSource.IsCancellationRequested)
+                    {
+                        Console.Write("Aborting process...");
+                        e.Cancel = true;
+                        cancellationTokenSource.Cancel();
+                    }
+                    else
+                    {
+                        Console.Write("Killing process without cleanup...");
+                    }
+                };
+                if (args.Length == 0)
+                {
+                    printHelp();
+                    return;
+                }
+                var input = "";
+                var options = new Dictionary<string, string>();
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("-"))
+                    {
+                        var option = args[i].Substring(1);
+                        if (hasParameter(option))
+                        {
+                            i++;
+                            options.Add(option, args[i]);
+                        }
+                        else
+                        {
+                            options.Add(option, "");
+                        }
+                    }
+                    else if (string.IsNullOrEmpty(input))
+                    {
+                        input = args[i];
+                    }
+                }
+                using (ConsoleProgress progress = new ConsoleProgress(options.ContainsKey("v")))
+                {
+                    if (args[0].ToLower() == "read")
+                    {
+                        if (string.IsNullOrEmpty(input))
+                            printReadHelp();
+                        else
+                            await read(input, progress, ct);
+                    }
+                }
+            }
         }
+
+        private IProgress<ProgressInfo> setupProgressReport(ASCIIProgressBar asciiprogress, bool verbose)
+        {
+            return new Progress<ProgressInfo>(progressInfo =>
+            {
+                if (!string.IsNullOrEmpty(progressInfo.line))
+                {
+                    if (!progressInfo.verbose || verbose)
+                        Console.WriteLine(progressInfo.line);
+                }
+                asciiprogress?.Report(progressInfo.progress / 100.0);
+            });
+        }
+
+        private static bool hasParameter(string option)
+        {
+            if (option.ToLower() == "d")
+                return true;
+            return false;
+        }
+
+        private static void printHelp()
+        {
+            Console.WriteLine("Help");
+        }
+
+        private static void printReadHelp()
+        {
+            Console.WriteLine("Read Help");
+        }
+        private static async Task read(string input, ConsoleProgress progress, CancellationToken ct)
+        {
+            PatchProcess patchProcess = new PatchProcess();
+            var mapDescriptors = await patchProcess.loadWbfsIsoFile(input, progress, ct);
+            foreach (var mapDescriptor in mapDescriptors)
+            {
+                mapDescriptor.ToMD();
+            }
+        }
+
     }
 }
---
+++ CSMM/ProgressReporting/ConsoleProgress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStreetMapManager
+{
+    public class ConsoleProgress : IDisposable, IProgress<ProgressInfo>
+    {
+        private ASCIIProgressBar asciiProgressBar;
+        private bool verbose;
+
+        public ConsoleProgress(bool verbose)
+        {
+            asciiProgressBar = new ASCIIProgressBar();
+            this.verbose = verbose;
+        }
+
+        public void Dispose()
+        {
+            asciiProgressBar.Dispose();
+        }
+
+        public void Report(ProgressInfo progressInfo)
+        {
+            if (!string.IsNullOrEmpty(progressInfo.line))
+            {
+                if (!progressInfo.verbose || verbose)
+                    Console.WriteLine(progressInfo.line);
+            }
+            asciiProgressBar?.Report(progressInfo.progress / 100.0);
+        }
+    }
+}
