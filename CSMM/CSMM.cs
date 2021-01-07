8a68fcba79cf8d9ba57752cc661c384c440f08ff
Deflaktor <deflaktor@gmail.com>
2021-01-07 08:35:01 +0100

    Add extract and export commands to cli

--- CSMM/CSMM.cs
+++ CSMM/CSMM.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace CustomStreetMapManager
 
         static async Task Main(string[] args)
         {
+            //args = new string[] { "export", "Test", "-d", "MD", "-i", "0", "-v", "-o" };
+
             using (var cancellationTokenSource = new CancellationTokenSource())
             {
                 var ct = cancellationTokenSource.Token;
@@ -30,7 +33,7 @@ namespace CustomStreetMapManager
                 };
                 if (args.Length == 0)
                 {
-                    printHelp();
+                    PrintHelp();
                     return;
                 }
                 var input = "";
@@ -40,7 +43,7 @@ namespace CustomStreetMapManager
                     if (args[i].StartsWith("-"))
                     {
                         var option = args[i].Substring(1);
-                        if (hasParameter(option))
+                        if (HasParameter(option))
                         {
                             i++;
                             options.Add(option, args[i]);
@@ -55,27 +58,64 @@ namespace CustomStreetMapManager
                         input = args[i];
                     }
                 }
-                using (ConsoleProgress progress = new ConsoleProgress(options.ContainsKey("v"), options.ContainsKey("q")))
+                var quiet = options.ContainsKey("q");
+                var verbose = options.ContainsKey("v");
+                using (ConsoleProgress progress = new ConsoleProgress(verbose, quiet))
                 {
-                    if (args[0].ToLower() == "read")
+                    try
                     {
-                        if (string.IsNullOrEmpty(input))
-                            printReadHelp();
-                        else
-                            await read(input, progress, ct);
+                        if (args[0].ToLower() == "json")
+                        {
+                            if (string.IsNullOrEmpty(input))
+                                printJsonHelp();
+                            else
+                                await Json(input, options.GetValueOrDefault("d", null), options.ContainsKey("p"), progress, ct);
+                        }
+                        if (args[0].ToLower() == "extract")
+                        {
+                            if (string.IsNullOrEmpty(input))
+                                printExtractHelp();
+                            else
+                                await Extract(input, options.GetValueOrDefault("d", null), progress, ct);
+                        }
+                        if (args[0].ToLower() == "export")
+                        {
+                            if (string.IsNullOrEmpty(input))
+                                printExportHelp();
+                            else
+                                await Export(input, options.GetValueOrDefault("d", null), options.GetValueOrDefault("i", null), options.ContainsKey("a"), options.GetValueOrDefault("n", null), options.ContainsKey("o"), progress, ct);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Environment.ExitCode = e.HResult;
+                        Console.WriteLine(e.Message);
+                        if (verbose)
+                        {
+                            throw;
+                        }
+                    }
+                    finally
+                    {
+                        // cleanup?
                     }
                 }
             }
         }
 
-        private static bool hasParameter(string option)
+        private static bool HasParameter(string option)
         {
-            if (option.ToLower() == "d")
-                return true;
+            switch (option.ToLower())
+            {
+                case "d":
+                case "i":
+                case "n":
+                    return true;
+            }
             return false;
         }
 
-        private static void printHelp()
+        private static void PrintHelp()
         {
             _ = @"
 --------------------------------------------------------------------------------
@@ -99,42 +139,147 @@ namespace CustomStreetMapManager
 usage: csmm <command> [options] <input>
 
 commands:
-   export          export one or several map descriptor files from an image or 
-                     file system
-   import          import one or several map descriptor files from an image or 
-                     file system
-   read            output the whole data from an image or file system in json
-   write           input a json file to write to an image or file system
+   extract         extracts the data of a Fortune Street game disc image to a 
+                     directory
+   export          export one or several map descriptor files (*.md) from a 
+                     Fortune Street game disc directory
+   import          import one or several map descriptor files (*.md) into a 
+                     Fortune Street game disc directory
+   pack            pack a Fortune Street game disc directory into an image file
+   json            output the whole data from a Fortune Street game disc 
+                     directory to json
 
 Use 'csmm <command>' to read about a specific command.
 
 options:
-   clone
    -v              verbose
-   -q              quiet
-   -d path         destination
-   -c              cleanup
-   --wiimmfi       patch wiimmfi (default)
-   --no-wiimmfi    do not patch wiimmfi
+   -q              quiet (overrides verbose)
 ");
             _ = @"
 --------------------------------------------------------------------------------
 ";
         }
+        private static void printExtractHelp()
+        {
+            _ = @"
+--------------------------------------------------------------------------------
+";
+            Console.WriteLine(@"
+usage: csmm extract [options] <input>
 
-        private static void printReadHelp()
-        {
-            Console.WriteLine("Read Help");
+options:
+   -v              verbose
+   -q              quiet (overrides verbose)
+
+   -d path         destination
+");
+            _ = @"
+--------------------------------------------------------------------------------
+";
         }
-        private static async Task read(string input, ConsoleProgress progress, CancellationToken ct)
+        private static async Task Extract(string input, string destination, ConsoleProgress progress, CancellationToken ct)
         {
+            if (string.IsNullOrEmpty(destination))
+                throw new ArgumentException("Destination not set");
+            if (File.Exists(destination))
+                throw new ArgumentException("Destination must be a directory");
+            var mapDescriptors = await PatchProcess.Load(input, progress, ct, destination);
+            await Task.Delay(500);
+        }
+        private static void printExportHelp()
+        {
+            _ = @"
+--------------------------------------------------------------------------------
+";
+            Console.WriteLine(@"
+usage: csmm export [options] <input>
+
+options:
+   -v              verbose
+   -q              quiet (overrides verbose)
+
+   -d path         destination directory
+   -a              all
+   -i id1,...      ids of the maps to export
+   -n name1,...    internal names of the map to export
+   -o              overwrite existing files
+");
+            _ = @"
+--------------------------------------------------------------------------------
+";
+        }
+        private static async Task Export(string input, string destination, string idList, bool all, string internalNameList, bool overwrite, ConsoleProgress progress, CancellationToken ct)
+        {
+            if (string.IsNullOrEmpty(idList) && string.IsNullOrEmpty(internalNameList) && !all)
+                throw new ArgumentException("No map descriptor to export");
+
+            var mapDescriptors = await PatchProcess.Load(input, progress, ct, input);
+            await Task.Delay(500);
+
+            List<string> ids = new List<string>();
+            List<string> internalNames = new List<string>();
+            if (!string.IsNullOrEmpty(idList))
+                ids = new List<string>(idList.Split(','));
+            if (!string.IsNullOrEmpty(internalNameList))
+                internalNames = new List<string>(internalNameList.Split(','));
+
+            var mapDescriptorsToExport = new List<MapDescriptor>();
+            for (int i = 0; i < mapDescriptors.Count; i++)
+            {
+                bool export = false;
+                if (all)
+                    export = true;
+                if (internalNames.Contains(mapDescriptors[i].InternalName))
+                    export = true;
+                if (ids.Contains(i.ToString()))
+                    export = true;
+                if (export)
+                    mapDescriptorsToExport.Add(mapDescriptors[i]);
+            }
+            if (mapDescriptorsToExport.Count > 1 && !string.IsNullOrEmpty(Path.GetExtension(destination)))
+            {
+                throw new ArgumentException("Multiple map descriptors are to be exported, however the given destination is a filename. Use a directory instead.");
+            }
+            foreach (var mapDescriptor in mapDescriptorsToExport)
+            {
+                PatchProcess.ExportMd(destination, input, mapDescriptor, overwrite, progress, ct);
+            }
+            await Task.Delay(500);
+        }
+
+        private static void printJsonHelp()
+        {
+            _ = @"
+--------------------------------------------------------------------------------
+";
+            Console.WriteLine(@"
+usage: csmm json [options] <input>
+
+options:
+   -v              verbose
+   -q              quiet (overrides verbose)
+
+   -d path         destination file
+   -p              print to console
+");
+            _ = @"
+--------------------------------------------------------------------------------
+";
+        }
+        private static async Task Json(string input, string destination, bool print, ConsoleProgress progress, CancellationToken ct)
+        {
+            if (!print && string.IsNullOrEmpty(destination))
+                throw new ArgumentException("Neither destination nor print is set. Nothing to do.");
             var mapDescriptors = await PatchProcess.Load(input, progress, ct);
+            await Task.Delay(500);
             var options = new JsonSerializerOptions
             {
                 WriteIndented = true
             };
-            var json = JsonSerializer.Serialize(mapDescriptors, options);
-            Console.WriteLine(json);
+            if (print)
+                Console.WriteLine(JsonSerializer.Serialize(mapDescriptors, options));
+            if (!string.IsNullOrEmpty(destination))
+                File.WriteAllBytes(destination, JsonSerializer.SerializeToUtf8Bytes(mapDescriptors, options));
         }
 
     }
